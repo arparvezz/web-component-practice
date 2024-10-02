be52b7d35460a5f6c002b79e7b51ca7cbc3bf425
arparvezz <arparvez50@gmail.com>
2024-10-03 01:41:04 +0600

    second web-component added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class UserCard extends HTMLElement{
-  
+
   constructor(){
     super();
     this.attachShadow({mode: 'open'})
@@ -13,8 +13,9 @@ class UserCard extends HTMLElement{
       <style>
         .user-card-wrapper{
           width: 300px;
-          border: 1px solid #efefef;
+          border: 1px solid #eaecee;
           border-radius: 5px;
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.1);
           overflow: hidden;
           padding: 20px;
         }
@@ -52,3 +53,41 @@ class UserCard extends HTMLElement{
 
 
 customElements.define('user-card',UserCard)
+
+
+
+class PrintItems extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode: 'open'})
+  }
+
+  connectedCallback(){
+    let listArray = this.dataset.list.split(' ');
+
+    this.shadowRoot.innerHTML = `
+    <style>
+      .list-wrapper{
+        max-width: 300px;
+        background: #d5d8dc;
+        padding: 10px;
+        border-radius: 5px;
+        display: flex;
+        gap: 25px;
+      }
+    .single-item{
+      padding: 5px 10px;
+      background: #eaecee;
+      color: #273746;
+      border-radius: 5px;
+    }
+    </style>
+    <div class="list-wrapper">
+     ${listArray.map(item => `<div class="single-item">${item}</div>`).join('')}
+    </div>
+    `
+  }
+}
+
+
+customElements.define('print-items',PrintItems)
